2e36bfd7ac0fa0213e859fa911fc01d1020b1a24
Alina Thor <alinisthor@gmail.com>
2025-10-16 09:38:26 +0200

    first commit

---
+++ Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAloneTjuvOchPolis
+{
+    internal class Person
+    {
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+
+        public Person()
+        {
+            Random random = new Random();
+            PositionX = random.Next(1, 101);
+            PositionY = random.Next(1, 26);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿namespace CodeAloneTjuvOchPolis
+{
+    internal class Program
+    {
+        static List<Person> persons = new List<Person>()
+        {
+            new Person(), new Person(), new Person(), new Person(),
+        };
+        static void Main(string[] args)
+        {
+            RenderGameBoard();
+
+           
+
+
+
+            Console.ReadLine();
+        }
+
+        private static void RenderGameBoard()
+        {
+            
+            for (int y = 0; y <= 27; y++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                for (int x = 0; x <= 102; x++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    if (y == 0 || y == 27)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        if (x == 0 || x == 102)
+                        {
+                            Console.Write("X");
+                        }
+                        else
+                        {
+                            foreach(Person p in persons)
+                            {
+                                if (p.PositionX == x && p.PositionY == y)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                                    Console.Write("C");
+                                }
+                               
+                            }
+                           
+                                
+                        }
+
+                    }
+
+                }
+                Console.WriteLine();
+                
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
